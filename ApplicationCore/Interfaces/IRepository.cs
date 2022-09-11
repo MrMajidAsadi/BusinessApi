c5949214716b22b480ddcaaab15636696e7062cd
Majid Asadi <eng.majidasadi@hotmail.com>
2022-09-11 15:11:16 +0430

    Infrastructure -> create ef repository

--- ApplicationCore/Interfaces/IRepository.cs
+++ ApplicationCore/Interfaces/IRepository.cs
@@ -2,8 +2,8 @@ namespace ApplicationCore.Interfaces;
 
 public interface IRepository<T> where T : class, IAggregateRoot
 {
-    Task Insert(T entity);
-    Task<T> GetAsync(int id);
-    Task Update(T entity);
-    Task Delete(T entity);
+    Task InsertAsync(T entity);
+    Task<T?> GetAsync(int id);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
 }
--- Infrastructure/Data/EfRepository.cs
+++ Infrastructure/Data/EfRepository.cs
@@ -1,26 +1,49 @@
+using ApplicationCore.Entities;
 using ApplicationCore.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 
-public class EfRepository<T> : IRepository<T> where T : class, IAggregateRoot
+public class EfRepository<T> : IRepository<T> where T : BaseEntity, IAggregateRoot
 {
-    public Task Delete(T entity)
+    private readonly OVitrinDbContext _db;
+    private readonly DbSet<T> _entities;
+
+    public EfRepository(OVitrinDbContext db, DbSet<T> entities)
     {
-        throw new NotImplementedException();
+        _db = db;
+        _entities = entities;
     }
 
-    public Task<T> GetAsync(int id)
+    public async Task DeleteAsync(T entity)
     {
-        throw new NotImplementedException();
+        if (entity is null)
+            throw new ArgumentNullException(nameof(entity));
+        
+        _entities.Remove(entity);
+        await _db.SaveChangesAsync();
     }
 
-    public Task Insert(T entity)
+    public async Task<T?> GetAsync(int id)
     {
-        throw new NotImplementedException();
+        return await _entities.FirstOrDefaultAsync(x => x.Id == id);
     }
 
-    public Task Update(T entity)
+    public async Task InsertAsync(T entity)
     {
-        throw new NotImplementedException();
+        if (entity is null)
+            throw new ArgumentNullException(nameof(entity));
+        
+        _db.Add(entity);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        if (entity is null)
+            throw new ArgumentNullException(nameof(entity));
+        
+        _entities.Update(entity);
+        await _db.SaveChangesAsync();
     }
 }
