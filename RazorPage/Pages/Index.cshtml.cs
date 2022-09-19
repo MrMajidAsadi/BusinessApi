6d1a8206fe4188ca46aa446cd78877aeaa1ac443
Majid Asadi <eng.majidasadi@hotmail.com>
2022-09-19 11:47:41 +0430

    RazorPage -> populate index with businesses

--- RazorPage/Pages/Index.cshtml.cs
+++ RazorPage/Pages/Index.cshtml.cs
@@ -1,19 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPage.Pages.Business;
 
 namespace RazorPage.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly HttpClient _httpClient;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, HttpClient httpClient)
     {
         _logger = logger;
+        _httpClient = httpClient;
     }
 
-    public void OnGet()
-    {
+    public List<BusinessDto> Businesses { get; set; }
 
+    public async Task<IActionResult> OnGetAsync()
+    {
+        var response = await _httpClient.GetAsync("https://localhost:7153/api/Businesses");
+        if (!response.IsSuccessStatusCode)
+            return NotFound();
+        
+        Businesses = await response.Content.ReadFromJsonAsync<List<BusinessDto>>();
+        return Page();
     }
 }
