11437a583affa3d7ac7f8e20f4dfc27679cc76f5
Majid Asadi <eng.majidasadi@hotmail.com>
2022-09-19 11:16:41 +0430

    Api -> post business pictures with business

--- Api/Controllers/BusinessesController.cs
+++ Api/Controllers/BusinessesController.cs
@@ -1,9 +1,11 @@
 using Api.Dtos.Businesses;
+using Api.Dtos.Pictures;
 using ApplicationCore.Entities;
 using ApplicationCore.Interfaces;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Controllers;
 
@@ -14,16 +16,19 @@ public class BusinessesController : ControllerBase
 {
     private readonly ILogger<BusinessesController> _logger;
     private readonly IRepository<Business> _businessRepository;
+    private readonly IRepository<Picture> _pictureRepository;
     private readonly IFileService _fileService;
 
     public BusinessesController(
         ILogger<BusinessesController> logger,
         IRepository<Business> businessRepository,
+        IRepository<Picture> pictureRepository,
         IFileService fileService)
     {
         _logger = logger;
         _businessRepository = businessRepository;
         _fileService = fileService;
+        _pictureRepository = pictureRepository;
     }
 
     [HttpPost]
@@ -43,6 +48,7 @@ public class BusinessesController : ControllerBase
     }
 
     [HttpGet("{id}")]
+    [AllowAnonymous]
     public virtual async Task<ActionResult<BusinessDto>> Get(int id)
     {
         var business = await _businessRepository.GetAsync(id);
@@ -50,7 +56,22 @@ public class BusinessesController : ControllerBase
         if (business is null)
             return NotFound();
         
-        return Ok(new BusinessDto { Id = business.Id, Name = business.Name, Description = business.Description });
+        var pictures = await _pictureRepository.GetAll()
+            .Where(p => p.BusinessId == id)
+            .Select(p => new PictureDto
+            {
+                Id = p.Id,
+                Url = p.VirtualPath
+            })
+            .ToListAsync();
+        
+        return Ok(new BusinessDto
+        {
+            Id = business.Id,
+            Name = business.Name,
+            Description = business.Description,
+            Pictures = pictures
+        });
     }
     
     [HttpPut("{id}")]
--- Api/Dtos/Businesses/BusinessDto.cs
+++ Api/Dtos/Businesses/BusinessDto.cs
@@ -1,11 +1,12 @@
+using Api.Dtos.Pictures;
+
 namespace Api.Dtos.Businesses;
 
 #nullable disable
 public class BusinessDto
 {
     public int Id { get; set; }
-
     public string Name { get; set; }
-    
     public string Description { get; set; }
+    public List<PictureDto> Pictures { get; set; }
 }
